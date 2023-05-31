bd2e3af3a54a33093a7a010823c65ea1277251ca
Jp Sia <johnpaulsia4@gmail.com>
2023-06-01 03:09:44 +0800

    refactor: remove avatar component and change it with image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,27 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import profilePic from '@/public/assets/profile-picture.jpg'
 
 import { socialLinks } from '@/config/social-links'
 import { cn } from '@/lib/utils'
 import { Icons } from '@/components/icons'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { buttonVariants } from '@/components/ui/button'
 
 export function Header() {
   return (
     <header className='space-y-6'>
-      <div className='max-w-fit'>
-        <Avatar className='h-20 w-20'>
-          <AvatarImage src='https://github.com/jjppsia.png' />
-          <AvatarFallback>JP</AvatarFallback>
-        </Avatar>
-      </div>
-      <h1 className='text-3xl font-bold tracking-tight'>
-        Software engineer 👨‍💻
-      </h1>
-      <p className='max-w-2xl opacity-80'>
-        I&apos;m a software engineer who&apos;s passionate about building really
-        cool stuff with code. I&apos;m dreaming of bringing ideas to reality and
-        creating meaningful solutions that could make a difference.
+      <Image
+        src={profilePic}
+        alt='My profile picture'
+        className='h-28 w-28 rounded-full border-2 border-primary'
+        style={{ objectFit: 'cover' }}
+      />
+      <h1 className='text-3xl font-bold tracking-tight'>Hey there! 👋🏻</h1>
+      <p className='max-w-xl opacity-80'>
+        I&apos;m <b>Jp Sia</b>, a software engineer who&apos;s passionate about
+        building really cool stuff with code. I&apos;m dreaming of bringing
+        ideas to reality and creating meaningful solutions that could make a
+        difference.
       </p>
       <div>
         <ul className='flex items-center space-x-4'>
