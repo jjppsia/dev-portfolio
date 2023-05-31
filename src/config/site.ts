18f890cb5a41a22f679948bc67c7251a96a5d54b
Jp Sia <johnpaulsia4@gmail.com>
2023-06-01 03:11:45 +0800

    feat: add site config

---
+++ src/config/site.ts
@@ -0,0 +1,27 @@
+import { SiteConfig } from '@/types'
+
+export const siteConfig: SiteConfig = {
+  name: 'Jp Sia',
+  description:
+    "I'm Jp Sia, a software engineer who's passionate about building really cool stuff with code. I&apos;m dreaming of bringing ideas to reality and creating meaningful solutions that could make a difference.",
+  url: 'https://www.jpsia.me',
+  keywords: [
+    'Jp Sia',
+    'Portfolio',
+    'Software Engineer',
+    'Fullstack Developer',
+    'Frontend Developer',
+    'Backend Developer',
+  ],
+  authors: [
+    {
+      name: 'Jp Sia',
+      url: 'https://www.jpsia.me',
+    },
+  ],
+  creator: 'Jp Sia',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+}
